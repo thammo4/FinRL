abb544e46bb21af9ae71ca00d74186b943731702
thammo4 <thammo4@uic.edu>
2023-10-06 22:06:26 -0400

    Practice backtesting DRL algorithm performance on historical data

--- examples/finrl_ex.py
+++ examples/finrl_ex.py
@@ -2,6 +2,7 @@ import pandas as pd;
 import numpy as np;
 import datetime;
 import itertools;
+import matplotlib.pyplot as plt
 import yfinance as yf;
 
 # Fetching/Processing Data
@@ -18,7 +19,21 @@ from finrl.config import INDICATORS, TRAINED_MODEL_DIR, RESULTS_DIR;
 from finrl.main import check_and_make_directories;
 from finrl.meta.env_stock_trading.env_stocktrading import StockTradingEnv;
 
+from pypfopt.efficient_frontier import EfficientFrontier;
+
 check_and_make_directories([TRAINED_MODEL_DIR]);
+
+
+
+# Backtesting trained agents
+import matplotlib.pyplot as plt;
+from stable_baselines3 import A2C, DDPG, PPO, SAC, TD3;
+
+from finrl.agents.stablebaselines3.models import DRLAgent;
+from finrl.config import INDICATORS, TRAINED_MODEL_DIR;
+from finrl.meta.env_stock_trading.env_stocktrading import StockTradingEnv;
+from finrl.meta.preprocessor.yahoodownloader import YahooDownloader;
+
 
 
 
@@ -274,64 +289,192 @@ if if_using_sac:
 
 
 
+#
+# PART THREE - Backtest Agent Performance + Compare to Baseline Metrics (e.g. Mean-Variance Optimized Strategy and DJIA Performance)
+#
+
+
+
+#
+# Upload trade_data.csv (from part 1 tutorial)
+#
+
+
+
+#
+# Upload trained agents to the same directory
+#
+
+
+
+#
+# Load trained agents (commented out because code from all three demo notebooks has been consolidated into this script)
+#
+
+# trained_a2c = A2C.load(TRAINED_MODEL_DIR + '/agent_a2c') if if_using_a2c else None;
+# trained_ddpg = DDPG.load(TRAINED_MODEL_DIR + '/agent_ddpg') if if_using_ddpg else None;
+# trained_ppo = PPO.load(TRAINED_MODEL_DIR + '/agent_ppo') if if_using_ppo else None;
+# trained_td3 = TD3.load(TRAINED_MODEL_DIR + "/agent_td3") if if_using_td3 else None
+# trained_sac = SAC.load(TRAINED_MODEL_DIR + "/agent_sac") if if_using_sac else None
+
+
+
+
+#
+# Evaluate out-of-sample performance
+#
+
+stock_dimension = len(trade['tic'].unique());
+state_space = 1 + 2*stock_dimension + len(INDICATORS)*stock_dimension;
+print('Stock Dimension: {}\t State Space:{}'.format(stock_dimension, state_space));
+
+buy_cost_list = sell_cost_list = [.001] * stock_dimension;
+num_stock_shares = [0]*stock_dimension;
+
+env_kwargs = {
+	'hmax': 100,
+	'initial_amount': 1000,
+	'num_stock_shares': num_stock_shares,
+	'buy_cost_pct': buy_cost_list,
+	'sell_cost_pct': sell_cost_list,
+	'state_space': state_space,
+	'stock_dim': stock_dimension,
+	'tech_indicator_list': INDICATORS,
+	'action_space': stock_dimension,
+	'reward_scaling': 1e-4
+};
+
+e_trade_gym = StockTradingEnv(df=trade_df, turbulence_threshold=70, risk_indicator='vix', **env_kwargs);
+
+
+# Test A2C
+df_account_value_a2c, df_actions_a2c = DRLAgent.DRL_prediction(
+	model = trained_a2c,
+	environment = e_trade_gym
+) if if_using_a2c else (None, None);
+
+
+# Test DDPG
+df_account_value_ddpg, df_actions_ddpg = DRLAgent.DRL_prediction(
+	model=trained_ddpg,
+	environment=e_trade_gym
+) if if_using_ddpg else (None, None);
+
+# Test PPO
+df_account_value_ppo, df_actions_ppo = DRLAgent.DRL_prediction(
+	model=trained_ppo,
+	environment=e_trade_gym
+) if if_using_ppo else (None, None);
+
+# Test TD3
+df_account_value_td3, df_actions_td3 = DRLAgent.DRL_prediction(
+	model=trained_td3,
+	environment=e_trade_gym
+) if if_using_td3 else (None, None);
+
+# Test SAC
+df_account_value_sac, df_actions_sac = DRLAgent.DRL_prediction(
+	model=trained_sac,
+	environment=e_trade_gym
+) if if_using_sac else (None, None);
+
+
+
+
+
+#
+# Construct Mean-Variance Optimized portfolio for comparison
+#
+
+def process_df_for_mvo (df):
+	return df.pivot(index='date', columns='tic', values='close');
+
+def StockReturnsComputing (StockPrice, Rows, Cols):
+	StockReturn = np.zeros([Rows-1, Cols]);
+	for j in range(Cols):
+		for i in range(Rows-1):
+			StockReturn[i,j] = ((StockPrice[i+1,j]-StockPrice[i,j]) / StockPrice[i,j]) * 100;
+
+	return StockReturn;
+
+
+#
+# Compute Mean-Variance Weights
+#
+
+StockData = process_df_for_mvo(train_df);
+TradeData = process_df_for_mvo(trade_df);
+
+
+#
+# Compute asset returns
+#
+
+ar_StockPrices = np.asarray(StockData);
+[Rows,Cols] = ar_StockPrices.shape;
+ar_Returns = StockReturnsComputing(ar_StockPrices, Rows, Cols);
+
+
+#
+# Compute mean/vcov returns
+#
+
+mean_Returns = np.mean(ar_Returns, axis=0);
+cov_Returns = np.cov(ar_Returns, rowvar=False);
+
+print('Mean returns of assets in k-portfolio 1\n', mean_Returns);
+print('VCov matrix of returns\n', cov_Returns);
+
+
+ef_mean = EfficientFrontier(mean_Returns, cov_Returns, weight_bounds=(0,.5));
+raw_weights_mean = ef_mean.max_sharpe();
+cleaned_weights_mean = ef_mean.clean_weights();
+mvo_weights = np.array([1000 * cleaned_weights_mean[i] for i in range(len(cleaned_weights_mean))]);
+
+LastPrice = np.array([1/p for p in StockData.tail(1).to_numpy()[0]]);
+InitialPortfolio = np.multiply(mvo_weights, LastPrice);
+
+PortfolioAssets = TradeData @ InitialPortfolio;
+MVO = pd.DataFrame(PortfolioAssets, columns=['Mean Var']);
 
 
 
 
 
 
+#
+# Add DJIA performance data for comparison
+#
+
+df_dji = YahooDownloader(start_date=TRADE_START_DATE, end_date=TRADE_END_DATE, ticker_list=['dji']).fetch_data();
+
+dji = pd.merge(df_dji['date'], df_dji['close'].div(df_dji['close'][0]).mul(1000), how='outer', left_index=True, right_index=True).set_index('date');
 
 
 
 
+#
+# Backtest trade strategy performance for each of the five DRL algorithms, MVO, and DJIA
+#
+
+df_result_a2c = df_account_value_a2c.set_index(df_account_value_a2c.columns[0]).rename(columns={'account_value':'a2c'});
+df_result_ddpg = df_account_value_ddpg.set_index(df_account_value_ddpg.columns[0]).rename(columns={'account_value':'ddpg'});
+df_result_ppo = df_account_value_ppo.set_index(df_account_value_ppo.columns[0]).rename(columns={'account_value':'ppo'});
+df_result_td3 = df_account_value_td3.set_index(df_account_value_td3.columns[0]).rename(columns={'account_value':'td3'});
+df_result_sac = df_account_value_sac.set_index(df_account_value_sac.columns[0]).rename(columns={'account_value':'sac'});
 
 
 
 
+#
+# Consolidate results into dataframe
+#
+
+result = pd.DataFrame({'a2c':df_result_a2c['a2c'], 'ddpg':df_result_ddpg['ddpg'], 'ppo':df_result_ppo['ppo'], 'td3':df_result_td3['td3'], 'sac':df_result_sac['sac'], 'mvo':MVO['Mean Var'], 'djia':dji['close']})
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#
+# Q - how to plot `result`?
+#
